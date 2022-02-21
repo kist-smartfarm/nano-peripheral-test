4772f671e45997c61cddda43fe6645d1d9afee59
gr3atchoi <credtiger96@gmail.com>
2022-02-21 13:14:15 +0900

    inital commit : added basic script files.

---
+++ test_all.py
@@ -0,0 +1,9 @@
+# testing script for CAMs peripherals of jetson nano
+# email: redtiger@kist.re.kr
+#  @KIST smartfarm center
+
+def main(): 
+    print('hello world')
+
+if __name__ == '__main__': 
+    main()
---
+++ test_motor.py
@@ -0,0 +1,7 @@
+# import Rpi.GPIO 
+
+PWM0 = 32 
+PWM2 = 33 
+
+if __name__ == "__main__": 
+    test_motor()
